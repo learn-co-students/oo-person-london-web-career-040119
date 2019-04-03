07c5497b0531e9b6039c83b955a25baf408f251e
Ivan Szebenszki <szib77@gmail.com>
2019-04-03 16:29:41 +0100

    instantiation done

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,24 @@
-# your code goes here
+class Person
+  attr_accessor :bank_account
+  attr_reader :name, :happiness, :hygiene
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def happiness=(number)
+    @happiness = number
+    @happiness = 0 if number < 0
+    @happiness = 10 if number > 10
+  end
+
+  def hygiene=(number)
+    @hygiene = number
+    @hygiene = 0 if number < 0
+    @hygiene = 10 if number > 10
+  end
+
+end
