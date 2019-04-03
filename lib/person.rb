610a649b25c1e5fac8c5d5ead1502f42897fabfc
github username <github email address>
2019-04-03 17:11:53 +0100

    done!

--- lib/person.rb
+++ lib/person.rb
@@ -21,4 +21,52 @@ class Person
     @hygiene = 10 if number > 10
   end
 
+  def happy?
+    return true if @happiness > 7
+    return false
+  end
+
+  def clean?
+    return true if @hygiene > 7
+    return false
+  end
+
+  def get_paid(salary)
+    @bank_account += salary
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    '♪ Rub-a-dub just relaxing in the tub ♫'
+  end
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    '♪ another one bites the dust ♫'
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(person,topic)
+
+    if topic == "politics"
+      self.happiness -= 2
+      person.happiness -= 2
+      'blah blah partisan blah lobbyist'
+    elsif topic == "weather"
+      self.happiness += 1
+      person.happiness += 1
+      'blah blah sun blah rain'
+    else
+      'blah blah blah blah blah'
+    end
+
+  end
+
 end
